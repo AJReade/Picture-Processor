64ea6f6b7ceeaf9aee69a2dca2f1a35563044922
ajr21 <ajr21@ic.ac.uk>
2022-01-19 19:19:13 +0000

    Completed non-extension

--- src/picture/Picture.java
+++ src/picture/Picture.java
@@ -4,6 +4,9 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A class that encapsulates and provides a simplified interface for manipulating an image. The
@@ -11,21 +14,15 @@ import java.io.IOException;
  */
 public class Picture {
 
-  /**
-   * The internal image representation of this picture.
-   */
+  /** The internal image representation of this picture. */
   private final BufferedImage image;
 
-  /**
-   * Construct a new (blank) Picture object with the specified width and height.
-   */
+  /** Construct a new (blank) Picture object with the specified width and height. */
   public Picture(int width, int height) {
     image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
   }
 
-  /**
-   * Construct a new Picture from the image data in the specified file.
-   */
+  /** Construct a new Picture from the image data in the specified file. */
   public Picture(String filepath) {
     try {
       image = ImageIO.read(new File(filepath));
@@ -40,7 +37,7 @@ public class Picture {
    * @param x the x co-ordinate of the point
    * @param y the y co-ordinate of the point
    * @return <tt>true</tt> if the point lies within the boundaries of the picture, <tt>false</tt>
-   * otherwise.
+   *     otherwise.
    */
   public boolean contains(int x, int y) {
     return x >= 0 && y >= 0 && x < getWidth() && y < getHeight();
@@ -97,7 +94,7 @@ public class Picture {
    * @param y y-coordinate of the pixel value to return
    * @return the RGB components of the pixel-value located at (x,y).
    * @throws ArrayIndexOutOfBoundsException if the specified pixel-location is not contained within
-   *                                        the boundaries of this picture.
+   *     the boundaries of this picture.
    */
   public Color getPixel(int x, int y) {
     int rgb = image.getRGB(x, y);
@@ -138,11 +135,11 @@ public class Picture {
   /**
    * Update the pixel-value at the specified location.
    *
-   * @param x   the x-coordinate of the pixel to be updated
-   * @param y   the y-coordinate of the pixel to be updated
+   * @param x the x-coordinate of the pixel to be updated
+   * @param y the y-coordinate of the pixel to be updated
    * @param rgb the RGB components of the updated pixel-value
    * @throws ArrayIndexOutOfBoundsException if the specified pixel-location is not contained within
-   *                                        the boundaries of this picture.
+   *     the boundaries of this picture.
    */
   public void setPixel(int x, int y, Color rgb) {
 
@@ -151,13 +148,11 @@ public class Picture {
         y,
         0xff000000
             | (((0xff & rgb.getRed()) << 16)
-            | ((0xff & rgb.getGreen()) << 8)
-            | (0xff & rgb.getBlue())));
+                | ((0xff & rgb.getGreen()) << 8)
+                | (0xff & rgb.getBlue())));
   }
 
-  /**
-   * Returns a String representation of the RGB components of the picture.
-   */
+  /** Returns a String representation of the RGB components of the picture. */
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
@@ -178,4 +173,172 @@ public class Picture {
     sb.append("\n");
     return sb.toString();
   }
-}
+
+  //  inverts an image
+  public void invert(String filepath) {
+    for (int x = 0; x < getWidth(); x++) {
+      for (int y = 0; y < getHeight(); y++) {
+        final Color color = getPixel(x, y);
+        setPixel(
+            x,
+            y,
+            new Color((255 - color.getRed()), (255 - color.getGreen()), (255 - color.getBlue())));
+      }
+    }
+    saveAs(filepath);
+  }
+  // greyscale
+  public void grayscale(String filepath) {
+    for (int x = 0; x < getWidth(); x++) {
+      for (int y = 0; y < getHeight(); y++) {
+        final Color color = getPixel(x, y);
+        final int colourAvg = (color.getRed() + color.getGreen() + color.getBlue()) / 3;
+        setPixel(x, y, new Color(colourAvg, colourAvg, colourAvg));
+      }
+    }
+    saveAs(filepath);
+  }
+
+//  rotation
+  public void rotate(String angle, String filepath) {
+    switch (angle) {
+      case "90" -> rotate90().saveAs(filepath);
+      case "180" -> rotate90().rotate90().saveAs(filepath);
+      case "270" -> rotate90().rotate90().rotate90().saveAs(filepath);
+    }
+  }
+  private Picture rotate90() {
+    Picture instance = new Picture(getHeight(), getWidth());
+    for (int x = 0; x < getWidth(); x++) {
+      for (int y = 0; y < getHeight(); y++) {
+        final Color color = getPixel(x, y);
+        instance.setPixel((getHeight() -1 -y), x, color);
+      }
+    }
+    return instance;
+  }
+
+//  filp
+  public void flip(String type, String filepath) {
+    switch (type) {
+      case "H" -> flipH().saveAs(filepath);
+      case "V" -> flipV().saveAs(filepath);
+    }
+  }
+
+  private Picture flipH() {
+    Picture instance = new Picture(getWidth(), getHeight());
+    for (int x = 0; x < getWidth(); x++) {
+      for (int y = 0; y < getHeight(); y++) {
+        final Color color = getPixel(x, y);
+        instance.setPixel(getWidth() - 1 - x, y, color);
+      }
+    }
+    return instance;
+  }
+
+  private Picture flipV() {
+    Picture instance = new Picture(getWidth(), getHeight());
+    for (int x = 0; x < getWidth(); x++) {
+      for (int y = 0; y < getHeight(); y++) {
+        final Color color = getPixel(x, y);
+        instance.setPixel(x, getHeight() - 1 - y, color);
+      }
+    }
+    return instance;
+  }
+
+//  Blend
+  public static void blend(List<String> pictures, String filepath) {
+    Picture blended = new Picture(minWidth(pictures), minHeight(pictures));
+    List<Picture> pictures1 = new ArrayList<Picture>();
+    for (String picture : pictures) {
+      Picture temp = new Picture(picture);
+      pictures1.add(temp);
+      }
+
+    for (int x = 0; x < minWidth(pictures); x++) {
+      for (int y = 0; y < minHeight(pictures); y++) {
+        int avgRed = 0;
+        int avgGreen = 0;
+        int avgBlue = 0;
+        for (Picture picture : pictures1){
+          final Color color = picture.getPixel(x, y);
+          avgRed = avgRed + color.getRed();
+          avgGreen = avgGreen + color.getGreen();
+          avgBlue = avgBlue + color.getBlue();
+        }
+        avgRed = avgRed / pictures1.size();
+        avgGreen = avgGreen / pictures1.size();
+        avgBlue = avgBlue / pictures1.size();
+        blended.setPixel(x, y, new Color(avgRed, avgGreen, avgBlue));
+      }
+    }
+
+    blended.saveAs(filepath);
+  }
+
+  private static int minHeight(List<String> pictures) {
+    int min = -1;
+    for (String picture : pictures){
+      Picture temp = new Picture(picture);
+      if (min == -1 || temp.getHeight() < min){
+        min = temp.getHeight();
+      }
+      }
+    return min;
+    }
+
+  private static int minWidth(List<String> pictures) {
+    int min = -1;
+    for (String picture : pictures){
+      Picture temp = new Picture(picture);
+      if (min == -1 || temp.getWidth() < min){
+        min = temp.getWidth();
+      }
+    }
+    return min;
+  }
+
+//  Blur
+  public void blur(String filepath) {
+    Picture blured = new Picture(getWidth(), getHeight());
+    for (int x = 0; x < getWidth(); x++) {
+      for (int y = 0; y < getHeight(); y++) {
+        if (x != 0 && x != (getWidth() - 1) && y != 0 && y != (getHeight() - 1)) {
+
+          List<Color> colors = new ArrayList<Color>();
+          for (int z = -1; z < 2; z++) {
+            for (int w = -1; w < 2; w++) {
+//              System.out.println(z + "+" + w);
+//              System.out.println(x + "+" + y);
+              final Color color = getPixel(x + z, y + w);
+              colors.add(color);
+            }
+          }
+
+          int avgRed = 0;
+          int avgGreen = 0;
+          int avgBlue = 0;
+
+          for (Color color : colors) {
+            avgRed = avgRed + color.getRed();
+            avgGreen = avgGreen + color.getGreen();
+            avgBlue = avgBlue + color.getBlue();
+          }
+          avgRed = avgRed / colors.size();
+          avgGreen = avgGreen / colors.size();
+          avgBlue = avgBlue / colors.size();
+
+          blured.setPixel(x, y, new Color(avgRed, avgGreen, avgBlue));
+
+        } else {
+          blured.setPixel(x, y, getPixel(x, y));
+        }
+      }
+    }
+    blured.saveAs(filepath);
+  }
+
+
+  }
---
+++ src/picture/demo.java
@@ -0,0 +1,7 @@
+package picture;
+
+public class demo {
+  public static void main(String[] args) {
+
+  }
+}
